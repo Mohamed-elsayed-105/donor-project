c9308b482c4e7549d21f710e61cbda84746b323e
hala hamad <halamohamad133@yahoo.com>
2019-08-26 08:46:58 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Donation JQ
+
+$("#myBtn1").click(function(){
+    $("#myForm1").slideToggle(500)
+})
+
+
+$("#myBtn2").click(function(){
+    
+    $("#myForm1").slideUp(500)
+    $("#myForm2").slideToggle(500)
+})
