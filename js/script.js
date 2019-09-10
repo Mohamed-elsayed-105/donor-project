b603bc96efc4dadca74aa707c91db18df7615d6b
mohamed-elsayed-105 <mohammed.fathyelsayed@gmail.com>
2019-09-10 20:54:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ $("#myBtn2").click(function(){
     $("#myForm2").slideToggle(500)
 })
 // Donation JQ hala
+// login button 
+
+let loginBtnA = document.getElementById('loginBtn');
+loginBtnA.onclick = function(){
+    document.getElementById('loginBtn').setAttribute("onclick","window.open('Saleh-index.html','_blank')");
+
+}
+//login button
+// nav donate click
+
+let navBtnA = document.getElementById('donate');
+navBtnA.onclick = function(){
+    document.getElementById('donate').setAttribute("onclick","window.open('hala-index.html','_blank')");
+
+}
+// nav donate click
+// nav activities click
+let actBtnA = document.getElementById('activities');
+actBtnA.onclick = function(){
+    document.getElementById('activities').setAttribute("onclick","window.open('Ahmed-ibrahim-index.html','_blank')");
+
+}
